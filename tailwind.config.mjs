8a9448e88c815861654a7b47a8b84c2376123b4f
sarahhuang1030 <sarahhuang1030@gmail.com>
2026-01-29 14:12:39 -0700

    edit tailwind.config file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
