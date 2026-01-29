bdaaa25540ac74fd023416b3357685d140ae0b1e
sarahhuang1030 <sarahhuang1030@gmail.com>
2026-01-29 09:59:54 -0700

    force-dynamic

--- app/displayStudents/page.js
+++ app/displayStudents/page.js
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 // import Image from "next/image";
 import Link from "next/link";
 //let unitDemos = ["1", "2", "3", "4", "5", "6"];
