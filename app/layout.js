9c4c36b3d36f6a3f26fcfd5a112c25710cac006d
sarahhuang1030 <sarahhuang1030@gmail.com>
2026-01-29 13:16:01 -0700

    adding tailwind style

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 
 const geistSans = Geist({
